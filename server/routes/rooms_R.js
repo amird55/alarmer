09744ab953771c1c1ae5361136b5fb190f46c097
amird55 <amir.dgn@outlook.com>
2023-04-21 12:44:20 +0300

    mechanism for - update:show

--- server/routes/rooms_R.js
+++ server/routes/rooms_R.js
@@ -14,7 +14,8 @@ router.get('/SaveDummy', (req, res) => {
     res.send('Saved Dummy');
 });
 router.get('/Add',(req, res) => {
-    res.render("roomAdd", {pageTitle:"הוספת חדר"
+    res.render("roomAdd", {pageTitle:"הוספת חדר",
+        item:{}
     });
 });
 router.post('/Add',(req, res) => {
@@ -31,5 +32,11 @@ router.get('/List',async (req, res) => {
     let room_data=await roomModel.find();
     res.render("roomList", {pageTitle:"ניהול חדרים",
         data:room_data
+    });
+});
+router.get('/Edit',async (req, res) => {
+    let item_data=await roomModel.findById(req.query.id);
+    res.render("roomAdd", {pageTitle:"עריכת חדר",
+        item:item_data
     });
 });
