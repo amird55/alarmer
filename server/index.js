6c9989e7ffdd3ffab0ad432ca93bd4c4d31279b1
amird55 <amir.dgn@outlook.com>
2023-04-21 10:15:21 +0300

    starting 1st crud

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const port = 5858;
+const app = express();
+app.use(express.json());
+app.set("view engine", "ejs");
+
+require('dotenv').config();
+
+mongoose.connect(process.env.mongoString);
+const database = mongoose.connection
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+//--- connecting routers ------------------------
+const rooms_rtr = require('./routes/rooms_R');
+app.use('/R', rooms_rtr);
+//------------------------------------------------
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port ${port}`);
+});
